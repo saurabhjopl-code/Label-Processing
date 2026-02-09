4793cb686da3781d6089c03aabac2dee69b70fe0
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-09 18:17:15 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,11 @@
+// ===============================
+// GLOBAL CONFIG
+// ===============================
+
+// Uniware Stock CSV (READ ONLY)
+export const UNIWARE_CSV_URL =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRnSjU9QBQiT7bGPsZrwOmJfGhO1POL7QgSY_YjFbkoA0uZ9M2Z_a7QBglOeZMQp4j-e1xEYLZfUg4V/pub?output=csv";
+
+// Image Master CSV (GitHub)
+export const IMAGE_CSV_URL = "./data/image_master.csv";
 
